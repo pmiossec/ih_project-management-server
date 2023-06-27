0a9f644d943edc5dda9fba5f143c02765b2fd9a7
Philippe Miossec <pmiossec@gmail.com>
2023-06-27 10:46:46 +0200

    better signup fileds verification

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -21,7 +21,7 @@ router.post("/signup", (req, res, next) => {
   const { email, password, name } = req.body;
 
   // Check if email or password or name are provided as empty strings
-  if (email === "" || password === "" || name === "") {
+  if (!email || !password || !name) {
     res.status(400).json({ message: "Provide email, password and name" });
     return;
   }
