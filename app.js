7e78a0cc8479229583c3943971fe922ce9842c08
Philippe Miossec <pmiossec@gmail.com>
2023-06-27 12:47:50 +0200

    Protect business routes

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("./db");
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+const { isAuthenticated } = require("./middleware/jwt.middleware");
 
 const app = express();
 
@@ -18,8 +19,8 @@ require("./config")(app);
 app.use("/api", require("./routes/index.routes"));
 
 app.use("/auth", require("./routes/auth.routes"));
-app.use("/api", require("./routes/project.routes"));
-app.use("/api", require("./routes/task.routes"));
+app.use("/api", isAuthenticated, require("./routes/project.routes"));
+app.use("/api", isAuthenticated, require("./routes/task.routes"));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
